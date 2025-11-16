0bd0e42c6a13dd466cd8379bb0fb5d69ddfd63e4
Tushar Chauhan <tusharchauhan0912@gmail.com>
2025-11-16 13:07:57 +0530

    finished the infra of client-auth

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,42 @@
-export default function Home() {
-  return <div>hi</div>;
+"use client";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+
+export default function HomePage() {
+  const router = useRouter();
+  const {data , isPending} = authClient.useSession();
+
+  if(isPending){
+    return (
+      <div>
+        <Spinner />
+      </div>
+    )
+  }
+
+  if(!data?.session && !data?.user){
+    router.replace("/signin");
+  }
+
+  return (
+  <div>
+    <div>{data?.user.image}</div>
+    <div>{data?.user.email}</div>
+    <div>{data?.user.emailVerified}</div>
+    <div>{data?.user.id}</div>
+    <div>{data?.user.name}</div>
+    <Button onClick={
+      ()=>{
+        authClient.signOut({
+          fetchOptions:{
+            onError: (ctx) => console.log(ctx),
+            onSuccess: (ctx) => router.push("/signin")
+          }
+        });
+      }
+    }></Button>
+  </div>
+  );
 }
