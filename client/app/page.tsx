cf4562be09205667b356b1489ea2046e253887ab
Tushar Chauhan <tusharchauhan0912@gmail.com>
2025-11-18 11:01:58 +0530

    Github heatmap setup --profile/main

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,6 +4,8 @@ import { Spinner } from "@/components/ui/spinner";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 
+import GitHubHeatmap from "@/components/GitHubHeatmap";
+
 export default function HomePage() {
   const router = useRouter();
   const {data , isPending} = authClient.useSession();
@@ -21,22 +23,30 @@ export default function HomePage() {
   }
 
   return (
-  <div>
-    <div>{data?.user.image}</div>
-    <div>{data?.user.email}</div>
-    <div>{data?.user.emailVerified}</div>
-    <div>{data?.user.id}</div>
-    <div>{data?.user.name}</div>
-    <Button onClick={
-      ()=>{
-        authClient.signOut({
-          fetchOptions:{
-            onError: (ctx) => console.log(ctx),
-            onSuccess: (ctx) => router.push("/signin")
-          }
-        });
-      }
-    }></Button>
+  // <div>
+  //   <div>{data?.user.image}</div>
+  //   <div>{data?.user.email}</div>
+  //   <div>{data?.user.emailVerified}</div>
+  //   <div>{data?.user.id}</div>
+  //   <div>{data?.user.name}</div>
+  //   <Button onClick={
+  //     ()=>{
+  //       authClient.signOut({
+  //         fetchOptions:{
+  //           onError: (ctx) => console.log(ctx),
+  //           onSuccess: (ctx) => router.push("/signin")
+  //         }
+  //       });
+  //     }
+  //   }></Button>
+  // </div>
+  <div className=" w-screen min-h-screen bg-black flex text-white ">
+    <div>
+
+    </div>
+    <div>
+      <GitHubHeatmap username={"tusharchauhan09"} />
+    </div>
   </div>
   );
 }
